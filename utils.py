fc00c3141d69a2d8c50f5d04d187123b05a55d3a
yura <yuriy.logosha@gmail.com>
2020-05-21 23:54:45 +0300

    Remove logger from utils.

--- utils.py
+++ utils.py
@@ -9,31 +9,10 @@ Version 1.0
 
 from html.parser import HTMLParser
 from datetime import datetime
-import logging
 import requests
 import json
 import os
 import xml.etree.ElementTree as ET
-
-""" Logger Configuration """
-
-
-default_logging_name = 'utils'
-default_logging_level = 20
-FORMAT = '%(asctime)-15s %(levelname)s %(message)s'
-formatter = logging.Formatter(FORMAT)
-# Create handlers
-c_handler = logging.StreamHandler()
-f_handler = logging.FileHandler('%s.log' % default_logging_name)
-
-# Create formatters and add it to handlers
-c_handler.setFormatter(formatter)
-f_handler.setFormatter(formatter)
-
-logging.basicConfig(format=FORMAT, level=default_logging_level, handlers=[c_handler, f_handler])
-logger = logging.getLogger(default_logging_name)
-logger.setLevel(default_logging_level)
-
 
 """ Errors, Exceptions """
 
